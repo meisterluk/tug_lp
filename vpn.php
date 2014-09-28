258f5c3157a0bc613305f4e9b646c559452fa6ac
Lukas Prokop <admin@lukas-prokop.at>
2014-09-29 01:13:56 +0200

    Update config screenshots.

--- vpn.php
+++ vpn.php
@@ -1,24 +1,23 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<!-- HTML 4 sucks. HTML 5 to go -->
+<!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8" />
     <title>prokls</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="creator" content="Lukas Prokop">
-    <meta name="description" content="User-Website of prokls">
-    <meta name="language" content="en">
+    <meta name="creator" content="Lukas Prokop" />
+    <meta name="description" content="User-Website of prokls" />
+    <meta name="language" content="en" />
 
-    <meta name="robots" content="all">
-    <link rel="lukas' weblog" title="MyBlog" href="http://lukas-prokop.at/blog/">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <meta name="robots" content="all" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
  </head>
 
  <body>
     <h1>VPN Configuration</h1>
     <p style="text-align:center">
-        <img src="img/vpn_config_xubuntu_gnome_network_manager_0.9.4.1.png"
+        <img src="img/vpn_config_xubuntu_gnome_network_manager_0.9.8.8.png"
             alt="VPN Client Configuration TU Graz Linux xubuntu Network Manager 0.9.4.1">
-    <p>
+    </p>
+    <p>You can also import this <a href="TU_Graz.pcf">pcf-file</a>.</p>
 
     <p>Do you own a TUG IP address?</p>
 <?php if (substr($_SERVER['REMOTE_ADDR'], 0, 7) == '129.27.') { ?>
@@ -44,7 +43,7 @@
     </p>
 
     <pre id="signature">Lukas Prokop
-student of <span class="s">Computer Science</span> and <span class="s">Software Development and Business Management</span>
+student of <span class="s">Computer Science</span>
 admi<!-- -->n<!-- @ -->@<!-- -->lukas&#45;prokop.at
 License: Emailware</pre>
  </body>
